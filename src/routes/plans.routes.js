fe428e4ff21f95a8faa741dd097913af60eb5eaf
Devika Mujgule <mujgule.d@northeastern.edu>
2022-10-08 00:30:25 -0400

    final working CRD

--- src/routes/plans.routes.js
+++ src/routes/plans.routes.js
@@ -1,11 +1,24 @@
 const express = require('express');
-
 const router = express.Router();
-
+const schema = require("../schema")
 const Redis = require('redis');
 const client = Redis.createClient();
 client.connect();
 
+function validation(ajvValidate) {
+  return (req, res, next) => {
+    const valid = ajvValidate(req.body);
+    if (!valid) {
+      console.log(`Bad Req`);
+      const errors = ajvValidate.errors;
+      res.status(400).json(errors);
+    } else {
+      console.log(`validation passed`);
+      next();
+    }
+
+  };
+}
 
 // //GET ALL Plans
 // router.get('/', (req, res) => {
@@ -21,50 +34,49 @@ router.get('/:id', async (req, res) => {
   const id = String(req.params.id)
   const planVal = await client.get(id);
   const jsonPlan = JSON.parse(planVal)
-  console.log(jsonPlan);
+  console.log('Get by Id working');
 
   if (planVal) {
-    res.status(201).send(jsonPlan);
+    res.status(200).send(jsonPlan);
   } else {
-    res.status(404).send('The Plan does not exist!');
+    res.status(404).send({ "message": `The Plan with ID: ${id} does not exist!` });
   }
 });
 
 //post a new plan
-router.post('/', async (req, res) => {
+router.post('/', validation(schema), async (req, res) => {
   console.log(`POST working`);
-  console.log(req.body);
 
   const plan = JSON.stringify(req.body);
   const objId = String(req.body.objectId);
 
 
   const exists = await client.exists(objId)
-  console.log(exists);
+  console.log('POST', exists, validation(plan));
 
   if (exists == 1) {
-    console.log('checid-', exists);
-    res.status(409).send(`Plan with ID:${objId} already exists!`);
+    console.log('checkid-', exists);
+    res.status(409).send({ "message": `Plan with id: ${objId} already exists!` });
   } else {
+    validation(plan)
     client.set(objId, plan);
-    res.status(200).send(`Plan added successfully! ID: ${objId}
-  ${plan}`);
+    res.status(201).send({ "message": "Plan added successfully!", "id": `${objId}` });
   }
 });
 
 
 //delete a plan by id 
 router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
+  const id = String(req.params.id);
   const exists = await client.exists(id)
-  console.log(exists);
+  console.log('deleeetee ', exists);
 
   if (exists == 0) {
     console.log('checid-', exists);
-    res.status(409).send(`Plan with ID:${objId} doesn't exists!`);
+    res.status(404).send({ "message": `Plan id: ${id} doesn't exists!` });
   } else {
     client.del(id);
-    res.status(200).send(`Plan ID: ${id} deleted successfully!`);
+    res.status(200).send({ "message": `Plan id: ${id} deleted successfully!` });
   }
 });
 
